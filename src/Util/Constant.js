15036a04f2c02f9ab7950c4bc1e409ad4a04953c
请叫我百米冲刺 <1576044139@qq.com>
2019-05-27 14:36:40 +0800

    修改请求链接为 https

--- src/Util/Constant.js
+++ src/Util/Constant.js
@@ -1,6 +1,6 @@
 export const API_KEY = "7c2dfb674b5a73d2ccf204c69a4ed44a"
 export const GAODE_LOCATION_KEY = 'ce468efe250c3cc7dc6f8e742a79ce3c'
-export const BASE_URL = "http://apis.baidu.com/netpopo/weather/query"
+export const BASE_URL = "https://apis.baidu.com/netpopo/weather/query"
 export const TYPHONE_URL = "https://earth.nullschool.net/";
 export const MENU_WIDTH = 200
 export const MENU_IMG_HEIGHT = 130
