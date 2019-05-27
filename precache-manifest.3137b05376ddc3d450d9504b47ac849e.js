f655705b1453e4567ca326df3bdf8cc3b0851891
请叫我百米冲刺 <1576044139@qq.com>
2019-05-27 12:54:19 +0800

    Updates

---
+++ precache-manifest.3137b05376ddc3d450d9504b47ac849e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b582b3a2923e1eb0e19293c6bdd3e2be",
+    "url": "/Weather-React/index.html"
+  },
+  {
+    "revision": "68e1c9a02370a9b1a930",
+    "url": "/Weather-React/static/css/main.4a516402.chunk.css"
+  },
+  {
+    "revision": "8b812c9b426b3218b69f",
+    "url": "/Weather-React/static/js/2.c0e080f5.chunk.js"
+  },
+  {
+    "revision": "68e1c9a02370a9b1a930",
+    "url": "/Weather-React/static/js/main.70445fc7.chunk.js"
+  },
+  {
+    "revision": "aed0707c7348b231f061",
+    "url": "/Weather-React/static/js/runtime~main.6dcfcbf7.js"
+  },
+  {
+    "revision": "bae481325d6ef0fe7e071fa33cfbddf4",
+    "url": "/Weather-React/static/media/icon_drawer.bae48132.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.69353edf6902bd5cbebc40ed627e30b9.js"
+  "/Weather-React/precache-manifest.3137b05376ddc3d450d9504b47ac849e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Weather-React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
